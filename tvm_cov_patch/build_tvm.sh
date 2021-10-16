3f9afe764549056a68c3545d800a08ae0cd18354
Tzer-AnonBot <tzer.anon@protonmail.com>
2021-10-16 13:07:36 -0500

    refine build script

--- tvm_cov_patch/build_tvm.sh
+++ tvm_cov_patch/build_tvm.sh
@@ -1,9 +1,11 @@
 set -e
 
 echo "Installing latest TVM... If it fails you may want to use earlier versions."
-git clone https://github.com/apache/tvm.git
+git clone https://github.com/apache/tvm.git --recursive
+cp -r tvm tvm-no-cov
+
+echo "Start building TVM with coverage instrumentation"
 cd tvm
-git submodule init && git submodule update --recursive
 patch -p1 < ../memcov4tvm.patch
 mkdir -p build && cd build
 echo "We use LLVM Sanitizers. Please ensure you have clang & compiler-rt installed."
@@ -13,4 +15,23 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DCMAKE_C_COMPILER=$(which clang) \
          -DFETCHCONTENT_QUIET=0
 make -j$(nproc)
-echo "Successfully installed TVM-cov! Please set PYTHONPATH=/path/to/tvm/python before use"
+cd ..
+echo "Successfully installed TVM-cov!"
+echo "To use TVM-cov, copy and paste the following shell statements:"
+echo "export TVM_HOME=$(pwd)"
+echo "export PYTHONPATH=$TVM_HOME/python"
+
+echo "Start building TVM without coverage instrumentation"
+cd ../tvm-no-cov
+mkdir -p build && cd build
+cp ../../tvm/cmake/config.cmake . # Sync cmake file with coverage version.
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DCMAKE_CXX_COMPILER=$(which clang++) \
+         -DCMAKE_C_COMPILER=$(which clang) \
+         -DFETCHCONTENT_QUIET=0
+make -j$(nproc)
+cd ..
+echo "Successfully installed TVM w/o coverage!"
+echo "To use TVM-cov, copy and paste the following shell statements:"
+echo "export TVM_HOME=$(pwd)"
+echo "export PYTHONPATH=$TVM_HOME/python"
